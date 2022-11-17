2c043e644781d60db5bc34195b4801786a13452f
Rahul1034 <91685948+Rahul1034@users.noreply.github.com>
2022-11-17 14:53:42 +0530

    Add files via upload

---
+++ jUnitFunctions.java
@@ -0,0 +1,10 @@
+package jUnitTestPackage;
+
+public class jUnitFunctions {
+	public int addnumbers(int a, int b) {
+		return a+b;
+	}
+	public String addString(String a, String b) {
+		return a+b;
+	}
+}
---
+++ testAddNumbers.java
@@ -0,0 +1,15 @@
+package jUnitTestPackage;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testAddNumbers {
+
+	@Test
+	public void test() {
+		jUnitFunctions junit = new jUnitFunctions();
+		int result = junit.addnumbers(52, 38);
+		assertEquals(90, result);
+	}
+
+}
